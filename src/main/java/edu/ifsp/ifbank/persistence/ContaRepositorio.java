a3e3e98e2f77380f696abf7193c2c8d6dd1c1771
Eduarda Santana <eduardasanttana16@gmail.com>
2026-06-07 14:42:52 -0300

    feat: insere repositorios.

---
+++ src/main/java/edu/ifsp/ifbank/persistence/ContaRepositorio.java
@@ -0,0 +1,10 @@
+package edu.ifsp.ifbank.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.ifsp.ifbank.modelo.Conta;
+
+public interface ContaRepositorio extends JpaRepository<Conta, Integer> {
+	
+}
+
---
+++ src/main/java/edu/ifsp/ifbank/persistence/MovimentacaoRepositorio.java
@@ -0,0 +1,9 @@
+package edu.ifsp.ifbank.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.ifsp.ifbank.modelo.Movimentacao;
+
+public interface MovimentacaoRepositorio extends JpaRepository<Movimentacao, Integer> {
+	
+}

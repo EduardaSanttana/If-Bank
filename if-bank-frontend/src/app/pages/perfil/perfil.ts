07a4c05d81e9b32f67957265e01c91585ef7bf7a
Melissa Silveira <melissa.silveira0000@gmail.com>
2026-06-28 23:02:23 -0300

    feat(perfil): cria página perfil

--- if-bank-frontend/src/app/pages/perfil/perfil.ts
+++ if-bank-frontend/src/app/pages/perfil/perfil.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { Usuario } from '../../usuario';
+import { UsuarioService } from '../../usuario-service';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './perfil.html',
-  styleUrl: './perfil.css',
+  styleUrl: './perfil.css'
 })
-export class Perfil {}
+export class Perfil implements OnInit {
+
+  private usuarioService = inject(UsuarioService);
+  protected authService = inject(AuthService);
+
+  usuario = signal<Usuario | null>(null);
+
+  ngOnInit(): void {
+
+    const usuarioAuth = this.authService.getUsuario();
+
+    if (!usuarioAuth) {
+      return;
+    }
+
+    this.usuarioService.getById(usuarioAuth.id)
+      .subscribe(usuario => {
+
+        this.usuario.set(usuario);
+
+      });
+
+  }
+
+  formatarValor(valor: number): string {
+
+    return valor.toLocaleString('pt-BR', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    });
+
+  }
+
+}
